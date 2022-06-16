6a5f25ff9300c338f7be13448c62f3fbc1a35755
David Ongaro <davidongaroh@gmil.com>
2022-06-16 10:59:23 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordVal= word.split('')
+  const revwordVal= wordVal.reverse()
+  revWord= revwordVal.join('')
+  if (word===revWord){
+    return true
+  }
+  else return false
 }
-
 /* 
   Add your pseudocode here
+  let len= word.length
+for(let i=0; i<len/2; i++) {
+  if (word[i]!== word[len-1-i]) {
+    return false;
+    
+  }
+  else true;
+}
 */
 
 /*
   Add written explanation of your solution here
+split the string 
+reverse the split string
+join the reversed string
+check whether the original string and reversed version are equal
 */
 
 // You can run `node index.js` to view these console logs
